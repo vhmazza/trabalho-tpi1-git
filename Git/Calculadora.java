636bf8b08915e4a880a7bfadc194fa1792eaa2cb
vhmazza <vhmazzarioli6@gmail.com>
2025-09-15 21:58:02 -0300

    Adicionar versão amissao

--- Git/Calculadora.java
+++ Git/Calculadora.java
@@ -2,15 +2,16 @@ public class Calculadora {
 
     /**
      * Executa uma operação matemática básica.
-     * @param operacao
+     * @param operacao Tipo da operação: "soma"
      * @param a Primeiro número
      * @param b Segundo número
      * @return Resultado da operação
      */
     public double calcular(String operacao, int a, int b) {
         switch (operacao.toLowerCase()) {
-            default:
-                throw new IllegalArgumentException("Operação inválida: " + operacao);
+            case "soma":
+                return a + b;
+            
         }
     }
 
@@ -18,7 +19,7 @@ public class Calculadora {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        calc.calcular( "", 0, 0 );
+        System.out.println("Soma: " + calc.calcular("soma", 10, 5));
     }
 }
 
